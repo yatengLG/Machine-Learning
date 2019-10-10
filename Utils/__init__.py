922086fb83123fc4f438ca1185296263ea4c5d4a
yatengLG <767624851@qq.com>
2019-10-10 11:48:47 +0800

    部分修改

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -2,5 +2,4 @@
 # @Author  : LG
 
 from Utils.Data_Norm import data_normer
-from Utils.Distance import euclidean_distance, chebyshev_distance, city_block_distance
-from Utils.Data_Reader import data_reader
+from Utils.Distance import *
